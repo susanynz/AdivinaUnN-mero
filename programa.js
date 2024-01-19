df334c94615e23dc0f3b634c23f4f864453a630a
susanynz <susbelynz@gmail.com>
2024-01-19 14:52:37 -0600

    mensaje consola

--- programa.js
+++ programa.js
@@ -1,12 +1,12 @@
 alert('Te daré diez intentos para adivinar el número secreto');
 
-let numeroMaximoPosible = 100;
-let secretNumber = Math.floor(Math.random()*100)+1;
+const numeroMaximoPosible = 100;
+let secretNumber = Math.floor(Math.random() * numeroMaximoPosible) + 1;
 let userNumber;
 let intentos = 1;
 let maximosIntentos = 10;
-//let palabraIntento = "intento";
-console.log(secretNumber)
+
+console.log(secretNumber);
 
 // Bucle while (Mientras mi condición no se cumpla, entonces)()
 while (userNumber != secretNumber) {
@@ -15,20 +15,21 @@ while (userNumber != secretNumber) {
   console.log(userNumber);
 
   if (userNumber == secretNumber) {
-    //se está usando un operador ternario para simplificar un condicional if/else. El signo ? significa if y los dos puntos son para ejemplificar else 
-    alert(`Lo lograste. En ${intentos} ${intentos == 1 ? "intento" : "intentos"}`);
+    // Utilizar un solo mensaje en la consola
+    console.log(`¡Felicidades! Adivinaste el número secreto. En ${intentos} ${intentos == 1 ? "intento" : "intentos"}`);
   } else {
     // Función anidada (otra función dentro de la función)
     if (userNumber < secretNumber) {
       alert("Ups, el número secreto es incorrecto. Te daré una pista: el número es mayor");
+      console.log("Ups, el número secreto es incorrecto");
     } else {
-      alert("Ups, el número secreto es incorrecto. Te daré una pista: el número es mayor");
+      alert("Ups, el número secreto es incorrecto. Te daré una pista: el número es menor");
+      console.log("Ups, el número secreto es incorrecto");
     }
   }
 
   // Contador de intentos si el usuario no acierta
-  intentos = intentos + 1;
-  palabraIntento = "intentos";
+  intentos++;
   // Verifica si se superó el número máximo de intentos
   if (intentos > maximosIntentos) {
     alert(`Superaste el número máximo de ${maximosIntentos} intentos, vuelve mañana`);
